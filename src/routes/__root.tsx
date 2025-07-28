63ed9dd264e94e68e9f8ab3fa3bb39413ea5fdc6
cremacious <chrismackall3@gmail.com>
2025-07-28 16:46:30 -0400

    Initial clerk setup

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,8 +6,20 @@ import {
   createRootRoute,
   HeadContent,
   Scripts,
+  Link,
+  useNavigate,
 } from '@tanstack/react-router';
 import '../styles/app.css';
+import { ChartColumnBigIcon } from 'lucide-react';
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from '@clerk/tanstack-react-start';
+import { Button } from '~/components/ui/button';
 
 export const Route = createRootRoute({
   head: () => ({
@@ -36,15 +48,61 @@ function RootComponent() {
 }
 
 function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
+  const navigate = useNavigate();
   return (
-    <html>
-      <head>
-        <HeadContent />
-      </head>
-      <body>
-        {children}
-        <Scripts />
-      </body>
-    </html>
+    <ClerkProvider>
+      <html>
+        <head>
+          <HeadContent />
+        </head>
+        <body>
+          <nav className="bg-primary p-4 h-20 text-white flex items-center justify-between">
+            <Link to="/" className="flex gap-1 items-center font-bold text-2xl">
+              <ChartColumnBigIcon className="text-lime-500" /> TanTracker
+            </Link>
+            <div>
+              <SignedOut>
+                <div className="text-white flex items-center">
+                  <Button asChild variant="link" className="text-white">
+                    <SignInButton />
+                  </Button>
+                  <div className="w-[1px] h-8 bg-zinc-700" />
+                  <Button asChild variant="link" className="text-white">
+                    <SignUpButton />
+                  </Button>
+                </div>
+              </SignedOut>
+              <SignedIn>
+                <UserButton
+                  showName
+                  appearance={{
+                    elements: {
+                      userButtonAvatarBox: {
+                        border: '1px solid white',
+                      },
+                      userButtonOuterIdentifier: {
+                        color: 'white',
+                      },
+                    },
+                  }}
+                >
+                  <UserButton.MenuItems>
+                    <UserButton.Action
+                      label="Dashboard"
+                      labelIcon={<ChartColumnBigIcon size={16} />}
+                      onClick={() => {
+                        navigate({ to: '/' });
+                      }}
+                    />
+                  </UserButton.MenuItems>
+                </UserButton>
+              </SignedIn>
+            </div>
+          </nav>
+          {children}
+          <Scripts />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
